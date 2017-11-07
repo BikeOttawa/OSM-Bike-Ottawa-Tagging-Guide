5b59f1f7f2058b527ce4ddbdcc67b518777d8a0d
Denis <carriere.denis@gmail.com>
2017-11-06 19:07:34 -0500

    Remove clean script

--- GeoJSON/clean.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-const glob = require('glob')
-const load = require('load-json-file')
-const write = require('write-json-file')
-
-glob.sync(path.join(__dirname, '**', '*.geojson')).forEach(filepath => {
-  const geojson = load.sync(filepath)
-  write.sync(filepath.replace('.geojson', '.json'), geojson)
-})
